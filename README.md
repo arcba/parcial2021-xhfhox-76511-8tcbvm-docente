88d195bf2bad6d79ff78b64f6689595c89733345
arcba <alejandroreyc@yahoo.com.ar>
2021-06-30 16:03:02 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# parcial2021-xhfhox-76511-8tcbvm-docente
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/parcial2021-xhfhox-76511-8tcbvm-docente)
---
+++ src/app/components/proveedores/proveedores.component.ts
@@ -0,0 +1,115 @@
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Proveedor } from '../../models/proveedor';
+import { ModalDialogService } from '../../services/modal-dialog.service';
+import { ProveedoresService } from '../../services/proveedores.service';
+
+@Component({
+  selector: 'app-proveedores',
+  templateUrl: './proveedores.component.html',
+  styleUrls: ['./proveedores.component.css']
+})
+export class ProveedoresComponent implements OnInit {
+  Titulo = 'Proveedores';
+  TituloAccionABMC = {
+    A: '(Agregar)',
+    B: '(Eliminar)',
+    M: '(Modificar)',
+    C: '(Consultar)',
+    L: '(Listado)'
+  };
+  AccionABMC = 'L'; // inicialmente inicia en el listado de articulos (buscar con parametros)
+  Mensajes = {
+    SD: ' No se encontraron registros...',
+    RD: ' Revisar los datos ingresados...'
+  };
+
+  Items: Proveedor[] = null;
+  submitted: boolean = false;
+
+  FormRegistro: FormGroup;
+
+  constructor(
+    public formBuilder: FormBuilder,
+    private proveedoresService: ProveedoresService,
+    private modalDialogService: ModalDialogService
+  ) {}
+
+  ngOnInit() {
+    this.FormRegistro = this.formBuilder.group({
+      PoveedorId: [0],
+      ProveedorRazonSocial: [
+        null,
+        [Validators.required, Validators.minLength(5), Validators.maxLength(50)]
+      ],
+      ProveedorCodigo: [
+        null,
+        [Validators.required, Validators.pattern('^\\d{1,10}$')]
+      ],
+      ProveedorFecha: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern(
+            '(0[1-9]|[12][0-9]|3[01])[-/](0[1-9]|1[012])[-/](19|20)[0-9]{2}'
+          )
+        ]
+      ]
+    });
+  }
+
+  Agregar() {
+    this.AccionABMC = 'A';
+    this.FormRegistro.reset({ PoveedorId: 0 });
+    this.submitted = false;
+    this.FormRegistro.markAsUntouched();
+  }
+
+  // Buscar segun los filtros, establecidos en FormRegistro
+  Buscar() {
+    //this.modalDialogService.BloquearPantalla();
+    this.proveedoresService
+      .get()
+      //this.articulosService.get("", null, this.Pagina)
+      .subscribe((res: any) => {
+        this.Items = res;
+        //this.modalDialogService.DesbloquearPantalla();
+      });
+  }
+
+  // grabar tanto altas como modificaciones
+  Grabar() {
+    this.submitted = true;
+    if (this.FormRegistro.invalid) {
+      return;
+    }
+
+    //hacemos una copia de los datos del formulario, para modificar la fecha y luego enviarlo al servidor
+    const itemCopy = { ...this.FormRegistro.value };
+
+    //convertir fecha de string dd/MM/yyyy a ISO para que la entienda webapi
+    var arrFecha = itemCopy.ProveedorFecha.substr(0, 10).split('/');
+    if (arrFecha.length == 3)
+      itemCopy.ProveedorFecha = new Date(
+        arrFecha[2],
+        arrFecha[1] - 1,
+        arrFecha[0]
+      ).toISOString();
+
+    // agregar post
+    if (this.AccionABMC == 'A') {
+      //this.modalDialogService.BloquearPantalla();
+      this.proveedoresService.post(itemCopy).subscribe((res: any) => {
+        this.Volver();
+        this.modalDialogService.Alert('Registro agregado correctamente.');
+        this.Buscar();
+        //this.modalDialogService.DesbloquearPantalla();
+      });
+    }
+  }
+  // Volver desde Agregar/Modificar
+  Volver() {
+    this.AccionABMC = 'L';
+  }
+}
